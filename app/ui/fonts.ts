59e13710f925dfc13f6c4d9415f7d92e46d3c142
Ryota <ryotadeki2004@icloud.com>
2024-04-28 16:44:15 +0900

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = { className: "some-class-name" };
+//上記のコード追加したけど、そもそもチュートリアル内でのミスなのか？？
